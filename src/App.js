a790ef9b09b6424686121286fd87c380c108030f
Fernando <fernandocasali8@gmail.com>
2026-01-16 03:55:31 -0300

    remove BOM from App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-﻿import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.css';
 import { API_BASE, apiFetch } from './api';
 import fallbackFavicon from './fallback-favicon.svg';
